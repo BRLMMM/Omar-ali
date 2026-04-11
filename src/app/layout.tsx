980f54d742c833ae04156ecf6dc2db9939d21dbc
BRLMMM <aliiomr16@gmail.com>
2026-04-11 06:17:40 +0200

    fixing hydration missmatch error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${spaceGrotesk.variable}`}>
+    <html lang="en" className={`${spaceGrotesk.variable}`} suppressHydrationWarning>
       <body className={`${spaceGrotesk.className} bg-[#050505] text-white antialiased`}>
         <SmoothScroll>{children}</SmoothScroll>
       </body>
